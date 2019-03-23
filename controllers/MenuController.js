4e7c743c295e1fcd13e61d7e43166d4c40f63080
Derya O. Kurin <dozdemir663@gmail.com>
2019-03-23 17:29:03 +0300

    checkpoint-4

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -4,42 +4,48 @@ let date = require('date-and-time');
 
 module.exports = class MenuController {
   constructor() {
-    this.mainMenuQuestions = [
-      {
-       type: "list",
-        name: "mainMenuChoice",
-        message: "Please choose from an option below: ",
-        choices: [
-          "Add new contact",
-          "Exit",
-          "Get date and time"
-        ]
-      }
-    ];
+    this.mainMenuQuestions = [{
+      type: "list",
+      name: "mainMenuChoice",
+      message: "Please choose from an option below: ",
+      choices: [
+        "Add new contact",
+        "View all contacts",
+        "Search for a contact",
+        "Get date and time",
+        "Exit"
+      ]
+    }];
     this.book = new ContactController();
-}
+  }
 
   main() {
     console.log(`Welcome to AddressBloc!`);
-     inquirer.prompt(this.mainMenuQuestions).then((response) => {
-       switch(response.mainMenuChoice){
-         case "Add new contact":
-           this.addContact();
-           break;
-         case "Exit":
-           this.exit();
-           break;
-         case "Get date and time":
-           this.getDate();
-           break;
-         default:
-           console.log("Invalid input");
-           this.main();
-       }
-     })
-     .catch((err) => {
-       console.log(err);
-     });
+    inquirer.prompt(this.mainMenuQuestions).then((response) => {
+        switch (response.mainMenuChoice) {
+          case "Add new contact":
+            this.addContact();
+            break;
+          case "View all contacts":
+            this.getContacts();
+            break;
+          case "Get date and time":
+            this.getDate();
+            break;
+          case "Search for a contact":
+            this.search();
+            break;
+          case "Exit":
+            this.exit();
+            break;
+          default:
+            console.log("Invalid input");
+            this.main();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   clear() {
@@ -59,6 +65,77 @@ module.exports = class MenuController {
     });
   }
 
+  getContacts() {
+    this.clear();
+
+    this.book.getContacts().then((contacts) => {
+      for (let contact of contacts) {
+        console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`);
+      }
+      this.main();
+    }).catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
+  search(){
+      inquirer.prompt(this.book.searchQuestions)
+      .then((target) => {
+       this.book.search(target.name)
+       .then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+         }
+
+        });
+     })
+     .catch((err) => {
+       console.log(err);
+       this.main();
+     });
+    }
+
+    showContact(contact){
+      this._printContact(contact);
+      inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+    }
+
+    _printContact(contact){
+      console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+      );
+    }
+
+
   exit() {
     console.log("Thanks for using AddressBloc!");
     process.exit();
@@ -76,5 +153,23 @@ module.exports = class MenuController {
   remindMe() {
     return "Learning is a life-long pursuit";
   }
+
+  delete(contact){
+     inquirer.prompt(this.book.deleteConfirmQuestions)
+     .then((answer) => {
+       if(answer.confirmation){
+         this.book.delete(contact.id);
+         console.log("contact deleted!");
+         this.main();
+       } else {
+         console.log("contact not deleted");
+         this.showContact(contact);
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+       this.main();
+     });
+   }
 
 };
